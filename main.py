a54969b5c4ae172bf9ae8dfbfe3475572e70df54
Jason Goebel <goebel.jason@gmail.com>
2018-01-06 13:58:37 -0600

    ADDS: login.html and route. ***WORKING***

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz_app:{}@localhost:8889/blogz'.format('f1nc53pM6fz0apb7ms')
 app.config['SQLALCHEMY_ECHO'] = True
+app.secret_key = 'woof'
 db = SQLAlchemy(app)
 
 from model import *
@@ -28,6 +29,34 @@ def get_post(get_by='id', target=''):
 def index():
     return redirect('/blog')
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+
+  errors = {}
+
+  if request.method == 'POST':
+    post_username = request.form['username']
+    post_password = request.form['password']
+
+    # Check for valid entry
+    if is_valid(post_username):  errors['username'] = is_valid(post_username)
+    if is_valid(post_password):  errors['password'] = is_valid(post_password)
+
+    if len(errors) > 0:
+        return render_template('login.html', location="Login",  username=post_username, errors=errors)
+
+    # Cheack db for user with password
+    if User.query.filter_by(username=post_username, password=post_password).first() is None:
+      errors['login'] = "User does not exist or password is wrong"
+
+    if len(errors) > 0:
+        return render_template('login.html', location="Login",  username=post_username, errors=errors)
+
+    session['user'] = post_username
+    return redirect('/newpost')
+
+  return render_template('login.html', location="Login", errors=errors)
+
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
@@ -36,6 +65,7 @@ def newpost():
     if request.method == 'POST':
         post_title = request.form['title']
         post_body = request.form['body']
+        owner = session['user']
 
         if is_valid(post_title):  errors['title'] = is_valid(post_title)
         if is_valid(post_body):  errors['body'] = is_valid(post_body)
@@ -43,7 +73,7 @@ def newpost():
         if len(errors) > 0:
             return render_template('newpost.html', location="New Post",  title=post_title, body=post_body, errors=errors)
         
-        new_post = Blog(post_title, post_body)
+        new_post = Blog(post_title, post_body, owner)
         db.session.add(new_post)
         db.session.commit()
 
